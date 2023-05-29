02a790b695889bd2e5aa4530cc955f9dbdac9bcc
Macbook Alex <macbook@MacBook-Pro-Macbook.local>
2023-05-29 18:16:46 +0300

    Seminar2 finished

---
+++ Seminar2.py
@@ -0,0 +1,78 @@
+# n = 5
+# i = 1
+# fact = 1
+# while i <= n:
+#     fact *= i
+#     i += 1
+    
+# print(fact)
+
+# Вычисление чисел Фибоначчи
+
+# A = int(input("Введите число A: "))
+
+# fib_prev = 0
+# fib_curr = 1
+# n = 2
+
+# while fib_curr < A:
+#     fib_next = fib_prev + fib_curr
+#     fib_prev = fib_curr
+#     fib_curr = fib_next
+#     n +=1
+# if fib_curr == A:
+#     print("Число", A, "является", n, "-ым числом Фибоначчи")
+# else:
+#     print("Число", A, "не является числом Фибоначчи и равно -1")
+ 
+# Функция для чисел Фибоначчи
+
+
+# def check_fib_position (num):
+
+#     fib_prev = 0
+#     fib_curr = 1
+#     n = 2
+
+#     while fib_curr < num:
+#         fib_next = fib_prev + fib_curr
+#         fib_prev = fib_curr
+#         fib_curr = fib_next
+#         n +=1
+#     if fib_curr == num:
+#         print("Число", num, "является", n, "-ым числом Фибоначчи")
+#     else:
+#         print("Число", num, "не является числом Фибоначчи и равно -1")
+#     return n
+
+# A = int(input("Введите число A: "))
+
+# print(check_fib_position(A))
+
+# Расчёт количества дней
+
+# temperatura = [-20, 30, -40, 50, 10, -10]
+# schet = 0
+# max_dlina = 0
+
+# for i in temperatura:
+#     if i > 0:
+#         schet += 1
+#         print("Счёт равен: ", schet)
+#     else:
+#         if schet > max_dlina:
+#             max_dlina = schet
+#             print("Максимум равен: ", max_dlina)
+#         schet = 0
+        
+# print("Максимальное количество дней: ", max_dlina)
+
+# Вес арбузов
+
+# whight = [5, 1, 6, 5, 9]
+# schet = 1
+# n = max
+# if n = max
+#     print("Бери себе " - max)
+# else:
+#     print("Бери тёще")
